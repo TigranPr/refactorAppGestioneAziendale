0dafd507a404188eb67a272088b4203df1cc370e
Tigran <137753420+TigranPr@users.noreply.github.com>
2025-02-07 12:25:19 +0100

    entità: like, commento e news

---
+++ services/gestioneComunity/src/main/java/com/gruppo3/gestioneComunity/GestioneComunityApplication.java
@@ -0,0 +1,13 @@
+package com.gruppo3.gestioneComunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneComunityApplication.class, args);
+	}
+
+}
