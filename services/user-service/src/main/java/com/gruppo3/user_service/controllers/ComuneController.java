5a3ad0f4e9fd39c6d1b4edd3d07c37fed590d934
Cramer <fabiocramerotti91@gmail.com>
2025-02-07 16:32:32 +0100

    controller messi

--- services/user-service/src/main/java/com/gruppo3/user_service/controllers/ComuneController.java
+++ services/user-service/src/main/java/com/gruppo3/user_service/controllers/ComuneController.java
@@ -1,4 +1,35 @@
 package com.gruppo3.user_service.controllers;
 
+import com.gruppo3.user_service.dto.request.ComuneRequest;
+import com.gruppo3.user_service.dto.response.EntityIdResponse;
+import com.gruppo3.user_service.entity.Comune;
+import com.gruppo3.user_service.services.ComuneService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comuni")
 public class ComuneController {
+    @Autowired
+    private ComuneService comuneService;
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Comune> getById(@PathVariable Long id) {
+        return new ResponseEntity<>(comuneService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Comune>> getAll() {
+        return new ResponseEntity<>(comuneService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<EntityIdResponse> create(@RequestBody @Valid ComuneRequest request) {
+        return new ResponseEntity<>(comuneService.createComune(request), HttpStatus.CREATED);
+    }
 }
