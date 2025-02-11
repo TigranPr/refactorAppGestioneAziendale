64b489a017294900bff197d0ea1a8082a02d6b84
DiegoBas17 <d.basili17@gmail.com>
2025-02-11 12:17:41 +0100

    risolto problema di comunicazione del utente-client

--- services/gestionePersonale/src/main/java/com/gruppo3/gestionePersonale/GestionePersonaleApplication.java
+++ services/gestionePersonale/src/main/java/com/gruppo3/gestionePersonale/GestionePersonaleApplication.java
@@ -2,10 +2,12 @@ package com.gruppo3.gestionePersonale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class GestionePersonaleApplication {
 
 	public static void main(String[] args) {
